ce8467c31e26a864575c6ef9700932ba8408aebe
Rafael Henrique Xavier Trombin <134017823+Rafalinker@users.noreply.github.com>
2023-08-07 20:40:05 -0300

    Prática sobre eventos em Javascript

---
+++ Udemy/Javascript/Eventos/javascript.js
@@ -0,0 +1,10 @@
+//O botçao "onclick" cria uma função caso você clique no botão
+//Vamos criar então essa função logo abaixo
+function mensagem(nome) {
+    alert("Ei, não clique em mim "+nome);
+}
+//lembrando que usando aspas se cria uma string
+//Vamos concatenar que seria a adição com o "+" apra juntar duas Strings
+//Em "onclcick" use também "onmouseover" que chama ao passar o mouse em cima do botão 
+
+//No fim pesquise mais sobre "eventos javascript" para dar uma olhada
