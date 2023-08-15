e855993539f44958429f16fade3be5e0b73c3d41
Rafael Henrique Xavier Trombin <134017823+Rafalinker@users.noreply.github.com>
2023-08-15 10:17:17 -0300

    Add files via upload

---
+++ Udemy/Javascript/Comentários/script.js
@@ -0,0 +1,6 @@
+//Os comentários basicamente são códigos sem execução que servem para organizar seu código e deixar mais autoexplicativo
+//São usa dos "//" para comentar somente uma linha, e "*/" para comentar mais de uma linha, lebramdo que esse comando preica ser fechado:
+/*ola
+mundo*/
+
+//Exemplo feito! ;)
