39476f230847c6630c3cacb637806edb9ae99fbc
Rafael Henrique Xavier Trombin <134017823+Rafalinker@users.noreply.github.com>
2023-08-15 10:08:19 -0300

    Add files via upload

---
+++ Udemy/Javascript/Introdução a Javascript/script.js
@@ -0,0 +1,5 @@
+/*Javascript é considerada uma linguagem client, ou seja que será processada é um cliente, por exemplo o GPS(maps) é feito em javascript
+Um bom costume é usar ";" no final de cada código.
+Vamos fazer um hello world em java:*/
+alert("Olá Mundo");
+//basicamente a função do alert é um comando que será visível, excelente para teste e prática.
