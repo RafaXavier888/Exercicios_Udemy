fbd99c56c512074c7dfb698db52a5a1e38ee2dce
Rafael Henrique Xavier Trombin <134017823+Rafalinker@users.noreply.github.com>
2023-08-15 12:00:25 -0300

    Add files via upload

---
+++ Udemy/Javascript/Operações matemáticas/script.js
@@ -0,0 +1,20 @@
+/*Na programação existem varias operações de soma, subtração, divisão etc que são cahamadas de operações matemáticas
+e não nescessariamente são usando números*/
+
+//vamos de um simples exemplo:
+var a = 2;
+var b = 3;
+//agora vamos criar uma operação e vamos atribuir o valor de a+b
+var soma = a+b;
+var sub = a-b;
+//multiplicação usamos o sinal de "*"
+var mult = a*b;
+//em divisão usamos o sinal "/"
+var div = a/b;
+//agora vamos imprimir todos os resultados usando "alert"
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
+
+//obs: variaveis são iniciadas somente com letras(podem ser o nome que quiser), sem acento, e se tiver números que ainda sim sejam iniciados somente com letras.
