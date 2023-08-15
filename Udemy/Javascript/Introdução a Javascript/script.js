3671746695de62cd6867e945f970968d4cbf5262
Rafael Henrique Xavier Trombin <134017823+Rafalinker@users.noreply.github.com>
2023-08-15 12:17:41 -0300

    Update script.js

--- Udemy/Javascript/Introdução a Javascript/script.js
+++ Udemy/Javascript/Introdução a Javascript/script.js
@@ -1,4 +1,4 @@
-/*Javascript é considerada uma linguagem client, ou seja que será processada é um cliente, por exemplo o GPS(maps) é feito em javascript
+/*Javascript é considerada uma linguagem client, ou seja que será processada em um cliente, por exemplo o GPS(maps) é feito em javascript
 Um bom costume é usar ";" no final de cada código.
 Vamos fazer um hello world em java:*/
 alert("Olá Mundo");
