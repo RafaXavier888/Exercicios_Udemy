518a7568ecb8d2aebff435d80117027de0603ec3
Rafael Henrique Xavier Trombin <134017823+Rafalinker@users.noreply.github.com>
2023-08-15 10:39:39 -0300

    Conceito de variavel

---
+++ Udemy/Javascript/Variáveis/script.js
@@ -0,0 +1,7 @@
+/*Primeiramente vamos nos perguntar o que é uma variável?
+R: é um espaço na memória(ou uma caixa) em que eu vou armazenar alguma informação. */
+//Por exemplo:
+var mensagem = "Olá mundo!";
+//o valor de "=" é um valor de atribuição, ou seja você está atribuindo o valor para a variável.
+//agora que atribuímos a mensagem dentro da caixa(variavel) vamos imprimi-la
+alert(mensagem);
